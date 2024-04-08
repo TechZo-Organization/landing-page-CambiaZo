f203caa7eb2f8b43bbaa3b4a268d052afae6976e
Joseph Huamani | Nerk <u20221a133@upc.edu.pe>
2024-04-08 14:47:35 -0500

    Feat: Create function mostrarMensaje()
    Create a showMessage() function to confirm the form registration.

--- scripts/script-burger.js
+++ scripts/script-burger.js
@@ -7,3 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+function mostrarMensaje() {
+    alert("¡Registro exitoso! Gracias por unirse al cambio :)");
+    return true;    
+}
